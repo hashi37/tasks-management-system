c3133a3f1c6bf11f51156f8bffb30823b77b2640
hanak <jakubkanah@gmail.com>
2021-03-23 21:54:12 +0100

    Wygląd i bootstrap

--- src/main/java/com/wszib/tasks/web/SecurityConfig.java
+++ src/main/java/com/wszib/tasks/web/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,19 +31,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
 
+    }
 
-/*        http.authorizeRequests()
-                .anyRequest()
-                .permitAll()
-                .and().csrf().disable();
-
-        http.formLogin()
-                .loginPage("/login")
-                .failureUrl("/login-error")
-                .and()
-                .logout()
-                .logoutSuccessUrl("/index");*/
-
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/bootstrap/**", "/styles/**");
     }
 
     @Autowired
--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO usertype(id, type) values
-(1, "ADMIN"),
-(2, "LEADER"),
-(3, "EMPLOYEE");
-
-INSERT INTO taskstate(id, name) values
-(1, "NEW"),
-(2, "IN_PROGRESS"),
-(3, "DONE");
